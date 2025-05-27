948f75176ff29a9bb1a94212cfc018a6a54b1354
Shafiqur1425 <shafiqurrajib@gmail.com>
2025-05-27 21:49:08 +0600

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,19 @@
+#1
+x = "ABRACADABRA"
+print(x[2])
+
+#2
+x = "The quick brown fox jumps over the lazy dog"
+print(x[10:19])
+
+#3
+A = "Bangla"
+B = "Amar"
+C = "Shonar"
+D = B + " "+ C + " " + A
+print(f"{B} {C} {A}")
+
+#4
+singer_name = input("Who is your favourite singer ? : ") #Nusrat Fateh Ali Khan
+song_name = input("What is the name of the popular song of the singer ? : ")  #Tumhein Dillagi Bhool Jani Paray Gi
+print(f"My favourite singer is {singer_name},whose most popular song is {song_name}") 
